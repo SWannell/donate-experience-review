24bd8b52d20d2693d8b94b3baaeda218d09b4440
Sami Wannell <swannell@redcross.org.uk>
2020-06-12 16:58:24 +0100

    From landing and from form plots

--- plot_funnel.py
+++ plot_funnel.py
@@ -31,20 +31,11 @@ df = df[df['appeal'].isin(bigappeal.index)]
 df[['formpage', 'payment']].drop_duplicates().sort_values(by='payment')
 
 payment_types = ['card', 'paypal', 'direct-debit']
-# for appeal in bigappeal.index:
-#for platform in ['card', 'paypal', 'direct-debit']:
-for appeal in ['Kindness Starts With You']:
-    print(df[df['appeal'] == appeal])
-    appeal_df = pd.DataFrame(index=['19-12', '20-01', '20-02', '20-03',
-                                    '20-04', '20-05'],
-                             columns=[''])
-
 months = sorted(df['month'].unique())
 
 cvr_by_platform = {k: None for k in payment_types}
 
 for platform in payment_types:
-#for platform in ['card']:
     by_month = pd.DataFrame(index=months)
     for appeal in bigappeal.index:
         _ = df[df['appeal'] == appeal]
@@ -52,23 +43,43 @@ for platform in payment_types:
         a.drop('appeal', axis=1, inplace=True)
         b = a.pivot_table(index='month', columns='formpage')
         for mth in b.index:
-            print(mth, platform)
             first_step = b['users'].max(axis=1).loc[mth]
             ty_step = b['users'].loc[mth, 'Thank you']
             formpct = ty_step/first_step
-            print('{}/{} = {:.0f}%'.format(ty_step, first_step, 100*formpct))
             by_month.loc[mth, appeal] = formpct
     cvr_by_platform[platform] = by_month
 
-fig, axs = plt.subplots(1, 3, sharey=True, figsize=(13, 5))
+figs, axs = plt.subplots(1, 3, sharey=True, figsize=(13, 5))
 for i, platform in enumerate(cvr_by_platform.keys()):
     platform_data = cvr_by_platform[platform]
     # Added these bizarre steps to get it to add xticklabels
     platform_data = platform_data.reset_index()
-    platform_data = platform_data.rename(columns={"index":"month"})
-    platform_data.plot(legend=None, ax=axs[i], marker='o', linestyle='None', xticks=platform_data.index)
+    platform_data = platform_data.rename(columns={"index": "month"})
+    platform_data.plot(legend=None, ax=axs[i], marker='o', linestyle='None',
+                       xticks=platform_data.index)
     axs[i].set_xticklabels(months)
     axs[i].set_title(platform)
     axs[i].yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
-plt.suptitle('Form conversion rate by payment type', fontsize=20)
+plt.suptitle('Form:TY conversion rate by appeal', fontsize=20)
 plt.savefig('Outputs\\CVR_from-form_payment-type.png')
+
+landing_cvr = pd.DataFrame(index=months)
+for appeal in bigappeal.index:
+    steps = {'Appeal Landing': 0, 'Thank you': 0}
+    for step in steps:
+        data = df[(df['formpage'] == step) & (df['appeal'] == appeal)]
+        data = data[['month', 'users']]
+        data.set_index('month', inplace=True)
+        data.columns = [appeal]
+        if len(data.index) != len(data.index.unique()):
+            data = data.groupby('month').sum()
+        steps[step] = data
+    cvr = steps['Thank you'] / steps['Appeal Landing']
+    landing_cvr = landing_cvr.join(cvr)
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+landing_cvr.plot(legend=None, marker='o', ax=ax, linestyle='None')
+plt.xticks(range(len(landing_cvr.index)), landing_cvr.index)
+ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+ax.set_title('Landing:TY conversion rate by appeal', fontsize=20)
+plt.savefig('Outputs\\CVR_from-landing.png')
