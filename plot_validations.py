0427357ea58701b91d2a3ac67cbd81883a1cbb8c
Sami Wannell <swannell@redcross.org.uk>
2020-06-15 18:51:31 +0100

    Plot validation errors

--- plot_validations.py
+++ plot_validations.py
@@ -6,6 +6,7 @@ Created on Mon Jun 15 17:06:07 2020
 """
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt; plt.style.use('ggplot')
 import matplotlib.ticker as mtick
 import pickle
@@ -18,6 +19,16 @@ df.reset_index(inplace=True, drop=True)
 df.columns = ['page', 'field', 'events', 'date']
 df['page'] = df['page'].str.replace('donate.redcross.org.uk', '')
 df['field'] = df['field'].str.replace('View_', '')
+df = df[~df['page'].str.contains('/appeal')]  # donate module validation
+df = df[~df['page'].str.contains('/appeal')]  # donate module validation
+df['field'] = df['field'].replace('', np.nan)
+df.dropna(inplace=True)
+
+df.to_csv('AmendedData\\FormValidations.csv')
+
+# =============================================================================
+# Personal details form
+# =============================================================================
 
 personal = df[df['page'].str.contains('personaldetails')]
 personal_all = personal.drop(['page', 'date'], axis=1).groupby('field').sum()
@@ -26,17 +37,39 @@ personal_all = personal_all / personal_total
 personal_all = personal_all[~personal_all.index.str.contains('delivery')]
 personal_all = personal_all[~personal_all.index.str.contains('addresses')]
 
-blocks = {0: ['form-change-amount-input'],
-          1: ['title', 'first-name', 'surname', 'email'],
-          2: [s for s in personal['field'].unique() if s.startswith('address_')],
-          3: [s for s in personal['field'].unique() if 'MC_' in s],
-          4: ['card_name']}
+order = ['form-change-amount-input', 'title', 'first-name', 'surname', 'email',
+         'address_1', 'address_2', 'address_3', 'address_town',
+         'address_country', 'address_postcode',
+         'MC_formDonationType', 'MC_formOrganisationName', 'card_name']
 
-for i in personal_all.index:
-    for k, v in blocks.items():
-        if i in v:
-            personal_all.loc[i, 'block'] = k
+mapping = {field: i for i, field in enumerate(order)}
+key = personal_all.index.map(mapping)
+personal_all = personal_all.iloc[key.argsort()]
 
-personal_all.sort_values(by='block')['events'].plot.bar()
+# =============================================================================
+# Confirm details form
+# =============================================================================
 
-personal_all.sort_values(by='events', ascending=False).plot.bar()
+confirm = df[df['page'].str.contains('confirmdetails')]
+confirm_all = confirm.drop(['page', 'date'], axis=1).groupby('field').sum()
+confirm_total = 2819
+confirm_all = confirm_all / confirm_total
+confirm_all = confirm_all[~confirm_all.index.str.contains('delivery')]
+confirm_all = confirm_all[~confirm_all.index.str.contains('addresses')]
+
+key = confirm_all.index.map(mapping)
+confirm_all = confirm_all.iloc[key.argsort()]
+
+# =============================================================================
+# Plot!
+# =============================================================================
+
+figs, axs = plt.subplots(1, 2, sharey=True, figsize=(10, 5))
+personal_all.plot.bar(ax=axs[0], legend=None)
+axs[0].set_title('Personal details')
+confirm_all.plot.bar(ax=axs[1], legend=None)
+axs[1].set_title('Confirm details')
+axs[0].yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+plt.suptitle('Form validation % by field', fontsize=20)
+figs.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.savefig('Outputs\\ValidationByField.png')
